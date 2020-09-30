aedce7f1a9feb0568ac3ec3dc0421a37b470b0a8
milouteeuwen <70379034+milouteeuwen@users.noreply.github.com>
2020-09-30 20:09:36 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,12 @@
+console.log("doe tie t?");
+
+var menu = document.getElementsByClassName("lala");
+var paginas = document.getElementsByClassName("hehe");
+
+menu.addEventListener("click", laatZien);
+
+function laatZien() {
+
+  paginas.classlist.toggle("go");
+
+}
